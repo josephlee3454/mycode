b3728171c29c47189ab0bfcb56f3a7527f93d2a4
Mona Lisa <monalisa@alta3.com>
2021-08-05 21:42:30 +0000

    added some cool api stuff always rember no fetches in python

---
+++ mtg/apiMTG01.py
@@ -0,0 +1,11 @@
+import requests
+
+API = "https://api.magicthegathering.io/v1/"
+
+def main():
+    resp = requests.get(f"{API}sets") 
+    
+    print(dir(resp))
+
+if __name__ == "__main__":
+    main()
